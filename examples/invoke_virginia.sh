b4a1f29f83865ccdd7b6be2f47248e3508f373f9
ssup2 <supsup5642@gmail.com>
2024-06-24 05:19:35 +0900

    update

---
+++ examples/invoke_virginia.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws bedrock-runtime invoke-model --no-verify-ssl --endpoint-url "https://virginia.runtime.bedrock.in" --model-id "anthropic.claude-3-sonnet-20240229-v1:0" --body "$(base64 -i payload.json)" response.json
--- main.tf
+++ main.tf
@@ -25,12 +25,24 @@ resource "aws_route53_zone" "private_bedrock" {
 
 resource "aws_route53_record" "bedrock_virginia" {
   zone_id = aws_route53_zone.private_bedrock.zone_id
-  name    = "virginia.bedrock.in"
+  name    = "virginia.runtime.bedrock.in"
   type    = "A"
 
   alias {
     name                   = aws_vpc_endpoint.virginia_bedrock.dns_entry[0].dns_name
     zone_id                = aws_vpc_endpoint.virginia_bedrock.dns_entry[0].hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "bedrock_oregon" {
+  zone_id = aws_route53_zone.private_bedrock.zone_id
+  name    = "oregon.runtime.bedrock.in"
+  type    = "A"
+
+  alias {
+    name                   = aws_vpc_endpoint.oregon_bedrock.dns_entry[0].dns_name
+    zone_id                = aws_vpc_endpoint.oregon_bedrock.dns_entry[0].hosted_zone_id
     evaluate_target_health = true
   }
 }
@@ -82,6 +94,29 @@ module "vpc_virginia" {
   manage_default_security_group = true
 }
 
+module "vpc_oregon" {
+  source = "terraform-aws-modules/vpc/aws"
+  providers = {
+    aws = aws.oregon
+  }
+
+  name = format("%s-oregon-vpc", local.name)
+
+  cidr            = local.vpc_cidr_oregon
+  azs             = local.azs_oregon
+  public_subnets  = [for k, v in local.azs_oregon : cidrsubnet(local.vpc_cidr_oregon, 8, k)]
+  private_subnets = [for k, v in local.azs_oregon : cidrsubnet(local.vpc_cidr_oregon, 8, k + 8)]
+  intra_subnets   = [for k, v in local.azs_oregon : cidrsubnet(local.vpc_cidr_oregon, 8, k + 16)]
+
+  enable_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  manage_default_network_acl    = true
+  manage_default_route_table    = true
+  manage_default_security_group = true
+}
+
 ## Peering
 resource "aws_vpc_peering_connection" "seoul_virginia" {
   provider = aws.seoul
@@ -98,16 +133,78 @@ resource "aws_vpc_peering_connection_accepter" "seoul_virginia" {
   auto_accept               = true
 }
 
+resource "aws_route" "vpc_seoul_to_virginia" {
+  provider = aws.seoul
+
+  route_table_id            = module.vpc_seoul.private_route_table_ids[0]
+  destination_cidr_block    = local.vpc_cidr_virginia
+  vpc_peering_connection_id = aws_vpc_peering_connection.seoul_virginia.id
+}
+
+resource "aws_route" "vpc_virginia_to_vpc_seoul" {
+  provider = aws.virginia
+
+  route_table_id            = module.vpc_virginia.private_route_table_ids[0]
+  destination_cidr_block    = local.vpc_cidr_seoul
+  vpc_peering_connection_id = aws_vpc_peering_connection.seoul_virginia.id
+}
+
+resource "aws_vpc_peering_connection" "seoul_oregon" {
+  provider = aws.seoul
+
+  vpc_id      = module.vpc_seoul.vpc_id
+  peer_vpc_id = module.vpc_oregon.vpc_id
+  peer_region = "us-west-2"
+}
+
+resource "aws_vpc_peering_connection_accepter" "seoul_oregon" {
+  provider = aws.oregon
+
+  vpc_peering_connection_id = aws_vpc_peering_connection.seoul_oregon.id
+  auto_accept               = true
+}
+
 ## Endpoint
+module "sg_virginia_bedrock_vpc_endpoint" {
+  source = "terraform-aws-modules/security-group/aws"
+  providers = {
+    aws = aws.virginia
+  }
+
+  name   = format("%s-bedrock-vpc-endpoint-sg", local.name)
+  vpc_id = module.vpc_virginia.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "https"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+}
+
 resource "aws_vpc_endpoint" "virginia_bedrock" {
   provider = aws.virginia
 
   vpc_id              = module.vpc_virginia.vpc_id
   service_name        = "com.amazonaws.us-east-1.bedrock-runtime"
   vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
+
+  security_group_ids = [module.sg_virginia_bedrock_vpc_endpoint.security_group_id]
 
   subnet_ids = module.vpc_virginia.private_subnets
+}
+
+resource "aws_vpc_endpoint" "oregon_bedrock" {
+  provider = aws.oregon
+
+  vpc_id              = module.vpc_oregon.vpc_id
+  service_name        = "com.amazonaws.us-west-2.bedrock-runtime"
+  vpc_endpoint_type   = "Interface"
+
+  subnet_ids = module.vpc_oregon.private_subnets
 }
 
 ## EC2
@@ -146,6 +243,7 @@ module "ec2_bedrock_client" {
   create_iam_instance_profile = true
   iam_role_policies = {
     AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    AmazonBedrockFullAccess = "arn:aws:iam::aws:policy/AmazonBedrockFullAccess"
   }
 }
 
